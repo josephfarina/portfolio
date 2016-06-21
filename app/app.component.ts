e9849bf8e3b7312a40b1d5102251fe5b476f0eb8
Joseph Farina <Jrf61194@gmail.com>
2016-06-20 23:31:54 -0400

    g

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,3 +1,6 @@
+//todo: weird bug that stops toggling the form after the third click
+// get it working and then create a list of ways to easily toggle all of the UI elements ill be creating -- this will be my portfolio
+
 import { Component } from '@angular/core';
 import { FormComponent } from './form/form.component'
 
