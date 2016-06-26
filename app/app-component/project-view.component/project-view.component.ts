271aafd39c3bc5c23d2cd9c63d6b077b42e7b317
Joseph Farina <Jrf61194@gmail.com>
2016-06-25 22:55:41 -0400

    file creation

---
+++ app/app-component/project-view.component/project-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-project-view',
+    templateUrl: 'project-view.component.html',
+    styleUrls: ['project-view.component.css']
+})
+
+export class ProjectViewComponent {
+    
+}
