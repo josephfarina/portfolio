a484eb91fe910c57a25f5f9250ddc186a281d69b
Joseph Farina <Jrf61194@gmail.com>
2016-06-27 22:26:05 -0400

    tictactoe component moving along

--- app/projects/tictactoe/tictactoe.component.spec.ts
+++ app/projects/tictactoe/tictactoe.component.spec.ts
@@ -21,14 +21,83 @@ describe('Testing Quote Component:', () => {
         TicTacToeComponent
     ]);
 
-    beforeEach(inject([TestComponentBuilder], (_tcb:any) => {
+    beforeEach(inject([TestComponentBuilder], (_tcb: any) => {
         tcb = _tcb
+
     }));
 
     it('should fulfill dependencies', injectAsync(
         [TestComponentBuilder], (tcb: any) => {
-            return tcb.createAsync(TicTacToeComponent).then((fixture: any) => {});
+            return tcb.createAsync(TicTacToeComponent).then((fixture: any) => { });
         }
     ));
 
+    describe('setPlayer():', () => {
+
+        it('should toggle between userOne and userTwo', (done: any) => {
+            tcb.createAsync(TicTacToeComponent).then((fixture: any) => {
+                let ttt = fixture.componentInstance;
+
+                ttt.currentPlayer = ttt.userOne;
+                ttt.setPlayer();
+                expect(ttt.currentPlayer).toBe(ttt.userTwo);
+                ttt.setPlayer();
+                expect(ttt.currentPlayer).toBe(ttt.userOne);
+
+                done();
+            })
+                .catch((e: any) => done.fail(e));
+        });
+
+        it('should toggle between userTwo and userOne', (done: any) => {
+            tcb.createAsync(TicTacToeComponent).then((fixture: any) => {
+                let ttt = fixture.componentInstance;
+
+                ttt.currentPlayer = ttt.userTwo;
+                ttt.setPlayer();
+                expect(ttt.currentPlayer).toBe(ttt.userOne);
+                ttt.setPlayer();
+                expect(ttt.currentPlayer).toBe(ttt.userTwo);
+
+                done();
+            })
+                .catch((e: any) => done.fail(e));
+        });
+
+    });
+
+    describe('setTile():', () => {
+
+        it('should toggle between userOne and userTwo', (done: any) => {
+            tcb.createAsync(TicTacToeComponent).then((fixture: any) => {
+                let ttt = fixture.componentInstance;
+
+                ttt.currentPlayer = ttt.userOne;
+                ttt.setPlayer();
+                expect(ttt.currentPlayer).toBe(ttt.userTwo);
+                ttt.setPlayer();
+                expect(ttt.currentPlayer).toBe(ttt.userOne);
+
+                done();
+            })
+                .catch((e: any) => done.fail(e));
+        });
+
+    });
+
 });
+
+export interface Player {
+    name: string,
+    score: number
+}
+
+export const userOne: Player = {
+    score: 0,
+    name: 'Player 1'
+}
+
+export const userTwo: Player = {
+    score: 0,
+    name: 'Player 2'
+}
--- app/projects/tictactoe/tictactoe.component.ts
+++ app/projects/tictactoe/tictactoe.component.ts
@@ -12,133 +12,89 @@ import { ROUTER_DIRECTIVES } from '@angular/router';
 export class TicTacToeComponent {
     private userOne: Player = {
         score: 0,
-        name: 'Player 1'
+        name: 'Player 1',
+        tileType: TileType.O
     }
     private userTwo: Player = {
         score: 0,
-        name: 'Player 2'
+        name: 'Player 2',
+        tileType: TileType.X
     }
+    private tileBoard: any[] = [
+        [TileType.null, TileType.null, TileType.null],
+        [TileType.null, TileType.null, TileType.null],
+        [TileType.null, TileType.null, TileType.null]
+    ];
     private currentPlayer: Player = this.userOne;
 
-    // TODO: change to enum type
-    private tileBoard: any[] =
-    [
-        [null, null, null],
-        [null, null, null],
-        [null, null, null]
-    ];
-
     setPlayer() {
-        console.log(this.currentPlayer, 'current');
-        console.log(this.userOne, 'useone');
-        console.log(this.userTwo, 'usetwo');
-        if (this.currentPlayer == this.userOne) {
+        if (this.currentPlayer === this.userOne) {
             this.currentPlayer = this.userTwo;
         }
         else if (this.currentPlayer == this.userTwo) {
             this.currentPlayer = this.userOne;
         }
-        
-        console.log(this.currentPlayer, 'current');
     }
 
     setTile(xaxis: number, yaxis: number) {
-        console.log(xaxis, yaxis);
-        let tile: string;
-        if (this.currentPlayer === this.userOne) { tile = 'O'; }
-        if (this.currentPlayer === this.userTwo) { tile = 'X'; }
+        // prevent tile from being toggled if it was already set
+        if (this.tileBoard[yaxis][xaxis] !== TileType.null) { return; }
 
         if (xaxis === 0) {
-            if (yaxis === 0) { this.tileBoard[0][0] = tile; }
-            else if (yaxis === 1) { this.tileBoard[1][0] = tile; }
-            else if (yaxis === 2) { this.tileBoard[2][0] = tile; }
+            if (yaxis === 0) { this.tileBoard[0][0] = this.currentPlayer.tileType; }
+            else if (yaxis === 1) { this.tileBoard[1][0] = this.currentPlayer.tileType; }
+            else if (yaxis === 2) { this.tileBoard[2][0] = this.currentPlayer.tileType; }
         }
         else if (xaxis === 1) {
-            if (yaxis === 0) { this.tileBoard[0][1] = tile; }
-            else if (yaxis === 1) { this.tileBoard[1][1] = tile; }
-            else if (yaxis === 2) { this.tileBoard[2][1] = tile; }
+            if (yaxis === 0) { this.tileBoard[0][1] = this.currentPlayer.tileType; }
+            else if (yaxis === 1) { this.tileBoard[1][1] = this.currentPlayer.tileType; }
+            else if (yaxis === 2) { this.tileBoard[2][1] = this.currentPlayer.tileType; }
         }
         else if (xaxis === 2) {
-            if (yaxis === 0) { this.tileBoard[0][2] = tile; }
-            else if (yaxis === 1) { this.tileBoard[1][2] = tile; }
-            else if (yaxis === 2) { this.tileBoard[2][2] = tile; }
+            if (yaxis === 0) { this.tileBoard[0][2] = this.currentPlayer.tileType; }
+            else if (yaxis === 1) { this.tileBoard[1][2] = this.currentPlayer.tileType; }
+            else if (yaxis === 2) { this.tileBoard[2][2] = this.currentPlayer.tileType; }
         }
+
+        this.checkForWinner(xaxis,yaxis);
         this.setPlayer();
     }
 
-
-
-    alert(x: any, y: any) {
-        console.log('x-axis', x, 'y-axis', y);
+    resetTileBoard() {
+        for (let i = 0; i < this.tileBoard.length; i++) {
+            for ( let j = 0; j < this.tileBoard[i].length; j++) {
+                this.tileBoard[i][j] = TileType.null;
+            }
+        }
     }
 
+
+    checkForWinner(xaxis:number, yaxis:number) {
+        // let ycount: number;
+        // let xcount: number;
+        // for (let x = 0; x < this.tileBoard.length; x++) {
+        //     if (this.tileBoard[yaxis][x] === this.currentPlayer.tileType) { xcount++; } 
+        //     console.log(xcount, 'xcount');
+        //     if ( xcount === 3) { return; }
+        // }
+
+        // for (let y = 0; y < this.tileBoard.length; y++) {
+        //     if (this.tileBoard[y][xaxis] === this.currentPlayer.tileType) { ycount++; } 
+        //     console.log(ycount, 'ycount');            
+        //     if ( ycount === 3) { return; }
+        // }    
+    }    
+
+}
+
+enum TileType {
+    X,
+    O,
+    null
 }
 
 export interface Player {
     name: string,
-    score: number
+    score: number,
+    tileType: TileType
 }
-
-
-
-// export class TicTacToe {
-
-
-//      private tileBoard: TileValue[] =
-//      [
-//           [’null’,’null’,’null’],
-//           [’null’,’null’,’null’],
-//           [’null’,’null’,’null’]
-//      ];
-
-//      setTile(location: string, player: Player) {
-//           let ties: number = 0;
-//      let tile: TileValue;
-//      if (player === this.userOne) { tile = TileValue[‘x’]; }
-//      if (player === this.userTwo) { tile = TileValue[‘o’]; }
-
-//      switch(location) {
-//           case ‘a1’:
-//                this.tileBoard[0][0] = tile;
-//                break;
-//           case ‘a2’:
-//                this.tileBoard[0][1] = tile;
-//                break;
-//           case ‘a3’:
-//                this.tileBoard[0][2] = tile;
-//                break;
-//           case ‘b1’:
-//                this.tileBoard[1][0] = tile;
-//                break;
-//           case ‘b2’:
-//                this.tileBoard[1][1] = tile;
-//                break;
-//           case ‘b3’:
-//                this.tileBoard[1][2] = tile;
-//                break;
-//           case ‘c1’:
-//                this.tileBoard[2][0] = tile;
-//                break;
-//           case ‘c2’:
-//                this.tileBoard[2][1] = tile;
-//                break;
-//           case ‘c3’:
-//                this.tileBoard[2][2] = tile;
-//                break;
-//      }
-//      }
-
-//      setWinner(player:Player?) {
-//           if (player === null) { this.ties++; }
-//           switch(player)
-//           {
-//                case this.userOne:
-//                     this.userOne.score++;
-//                     break;
-//                case this.userOne:
-//                     this.userTwo.score++;
-//                     break;
-//           }
-//      }
-
-// }
