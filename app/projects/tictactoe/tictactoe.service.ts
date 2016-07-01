c146a9cb4464e0d36067fc5b2cbe2276390ce275
Joseph Farina <Jrf61194@gmail.com>
2016-07-01 18:19:29 -0400

    tic tac toe service and spec working

--- app/projects/tictactoe/tictactoe.service.ts
+++ app/projects/tictactoe/tictactoe.service.ts
@@ -2,33 +2,63 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class TicTacToeService {
-/* 
-TODO: 
-Turn all shared methods and attrs into this service and share them across components so that 
-create getters and setters for everything
+  private userOne: Player = {
+    score: 0,
+    name: 'Player 1',
+    tileType: TileType.O
+  };
+  private userTwo: Player = {
+    score: 0,
+    name: 'Player 2',
+    tileType: TileType.X
+  };
 
-*/
+  toggleTile(): void {
+    let onePrevTile: TileType = this.userOne.tileType;
+    let twoPrevTile: TileType = this.userTwo.tileType;
+    this.userOne.tileType = twoPrevTile;
+    this.userTwo.tileType = onePrevTile;
+  }
 
-    private userOne: Player;
-    private userTwo: Player;
-    getUserOne() {
-
+  setUserOne(name: string, tile: TileType): void {
+    if (name != null && name.length > 0) {
+      this.userOne.name = name;
     }
 
-    getUserTwo() {
-
+    if (tile !== this.userOne.tileType) {
+      this.toggleTile();
     }
 
+  }
+
+  getUserOne(): Player {
+    return this.userOne;
+  }
+
+  setUserTwo(name: string, tile: TileType): void {
+    if (name != null && name.length > 0) {
+      this.userTwo.name = name;
+    }
+
+    if (tile !== this.userTwo.tileType) {
+      this.toggleTile();
+    }
+
+  }
+
+  getUserTwo(): Player {
+    return this.userTwo;
+  }
 }
 
 export interface Player {
-    name: string;
-    score: number;
-    tileType: TileType;
+  name: string;
+  score: number;
+  tileType: TileType;
 }
 
 export enum TileType {
-    X,
-    O,
-    null
+  X,
+  O,
+  null
 }
