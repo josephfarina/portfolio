4f7d544dff2625fe374621880a0235bf379c1c95
Joseph Farina <Jrf61194@gmail.com>
2016-07-13 23:15:17 -0400

    almost working

--- app/projects/drum-machine/drum-machine.component.ts
+++ app/projects/drum-machine/drum-machine.component.ts
@@ -1,10 +1,21 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { DrumMachineService } from './drum-machine.service';
 @Component({
     selector: 'my-drum-machine',
-    template: ''
+    template: `
+    <h1>my drum machine</h1>
+    <button (click)="start()">start</button>
+    <button (click)="stop()">stop</button>
+    `,
+    providers: [DrumMachineService]
 })
 
 export class DrumMachineComponent {
     constructor(private drumMachineService: DrumMachineService) {}
+    start() {
+        this.drumMachineService.handlePlay();
+    }
+    stop() {
+        this.drumMachineService.handleStop();
+    }
 }
