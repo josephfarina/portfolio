b8271906d306bf3d320b02a2d12cc5106062b85f
Joseph Farina <Jrf61194@gmail.com>
2016-07-15 20:04:56 -0400

    changes to sequencerlinup

--- app/projects/drum-machine/sequencerLineUp.ts
+++ app/projects/drum-machine/sequencerLineUp.ts
@@ -1,139 +1,217 @@
 export let SEQUENCER_LINEUP: Object = {
-        '0' : {
-            'kick' : true,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
+    'settings': {
+        'kick': {
+
         },
-        '1' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
+        'snare': {
+
         },
-        '2' : {
-            'kick' : false,
-            'snare' : true,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
+        'lowtom': {
+
         },
-        '3' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
+        'midtom': {
+
         },
+        'hitom': {
+
+        },
+        'rimshot': {
+
+        },
+        'clap': {
+
+        },
+        'hihat': {
+
+        },
+        'cymbal': {
+
+        }
+    },
+    '0': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '1': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '2': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '3': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
 
 
 
-        '4' : {
-            'kick' : true,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '5' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '6' : {
-            'kick' : false,
-            'snare' : true,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '7' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
+    '4': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '5': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '6': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '7': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
 
 
 
-        '8' : {
-            'kick' : true,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '9' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '10' : {
-            'kick' : false,
-            'snare' : true,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '11' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
+    '8': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '9': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '10': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '11': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
 
 
 
-        '12' : {
-            'kick' : true,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '13' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '14' : {
-            'kick' : false,
-            'snare' : true,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-        '15' : {
-            'kick' : false,
-            'snare' : false,
-            'hihat': false,
-            'tomtom1': false,
-            'tomtom2': false,
-            'cymbal': false
-        },
-    };
+    '12': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '13': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '14': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+    '15': {
+        'kick': false,
+        'snare': false,
+        'lowtom': false,
+        'midtom': false,
+        'hitom': false,
+        'rimshot': false,
+        'clap': false,
+        'hihat': false,
+        'cymbal': false
+    },
+};
+
