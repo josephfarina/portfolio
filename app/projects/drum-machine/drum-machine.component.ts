6ccce23492bc6921d677e00589bbcfdb60053e14
Joseph Farina <Jrf61194@gmail.com>
2016-07-17 19:13:04 -0400

    working really well setup temp interface

--- app/projects/drum-machine/drum-machine.component.ts
+++ app/projects/drum-machine/drum-machine.component.ts
@@ -11,24 +11,8 @@ import { SEQUENCER_LINEUP } from './sequencerLineUp';
 
 export class DrumMachineComponent implements OnInit {
     private sequencerLineUp: Object = SEQUENCER_LINEUP;
-    private _tempo: number = 120;
-
-    private kick_0: boolean = false;
-    private kick_1: boolean = false;
-    private kick_2: boolean = false;
-    private kick_3: boolean = false;
-    private kick_4: boolean = false;
-    private kick_5: boolean = false;
-    private kick_6: boolean = false;
-    private kick_7: boolean = false;
-    private kick_8: boolean = false;
-    private kick_9: boolean = false;
-    private kick_10: boolean = false;
-    private kick_11: boolean = false;
-    private kick_12: boolean = false;
-    private kick_13: boolean = false;
-    private kick_14: boolean = false;
-    private kick_15: boolean = false;
+    private instruments: string[] = ['kick', 'snare', 'lowtom', 'midtom', 'hitom', 'rimshot', 'clap', 'hihat', 'cymbal'];
+    private currentType: string = 'kick';
 
     constructor(private metronomeService: DrumMachineMetronomeService) {
         this.metronomeService.setSequencerLineUp(this.sequencerLineUp);
@@ -36,36 +20,13 @@ export class DrumMachineComponent implements OnInit {
     }
     start() { this.metronomeService.play(); }
     stop() { this.metronomeService.stop(); }
-
-    ngOnInit() {
-        console.log(this.sequencerLineUp[0]);
-       this.metronomeService.play();
-        this.changeTempo();
-    }
-
-    changeTempo() {
-        this.sequencerLineUp['projectSettings']['tempo'] = this._tempo;
+    update() {
         this.metronomeService.setSequencerLineUp(this.sequencerLineUp);
-    }
+        console.log(this.sequencerLineUp); }
 
-    updateKick() {
-        this.sequencerLineUp['rhythmSettings']['0']['kick'] = this.kick_0;
-        this.sequencerLineUp['rhythmSettings']['1']['kick'] = this.kick_1;
-        this.sequencerLineUp['rhythmSettings']['2']['kick'] = this.kick_2;
-        this.sequencerLineUp['rhythmSettings']['3']['kick'] = this.kick_3;
-        this.sequencerLineUp['rhythmSettings']['4']['kick'] = this.kick_4;
-        this.sequencerLineUp['rhythmSettings']['5']['kick'] = this.kick_5;
-        this.sequencerLineUp['rhythmSettings']['6']['kick'] = this.kick_6;
-        this.sequencerLineUp['rhythmSettings']['7']['kick'] = this.kick_7;
-        this.sequencerLineUp['rhythmSettings']['8']['kick'] = this.kick_8;
-        this.sequencerLineUp['rhythmSettings']['9']['kick'] = this.kick_9;
-        this.sequencerLineUp['rhythmSettings']['10']['kick'] = this.kick_10;
-        this.sequencerLineUp['rhythmSettings']['11']['kick'] = this.kick_11;
-        this.sequencerLineUp['rhythmSettings']['12']['kick'] = this.kick_12;
-        this.sequencerLineUp['rhythmSettings']['13']['kick'] = this.kick_13;
-        this.sequencerLineUp['rhythmSettings']['14']['kick'] = this.kick_14;
-        this.sequencerLineUp['rhythmSettings']['15']['kick'] = this.kick_15;
-        this.metronomeService.setSequencerLineUp(this.sequencerLineUp);
-    }
+    ngOnInit() { this.metronomeService.play(); }
+
 
 }
+
+
--- app/projects/drum-machine/sequencerLineUp.ts
+++ app/projects/drum-machine/sequencerLineUp.ts
@@ -1,251 +1,247 @@
 export let SEQUENCER_LINEUP: Object = {
-
-    'projectSettings': {
-        'tempo': 50,
-        'volume': .5,
-        'reverb': 1
+  'projectSettings': {
+    'tempo': 66,
+    'volume': 1,
+    'reverb': 1
+  },
+  'instrumentSettings': {
+    'kick': {
+      'level': 1,
+      'attack': 0,
+      'decay': 0.5,
+      'distortion': 1
     },
-
-    'instrumentSettings': {
-        'kick': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'snare': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'lowtom': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'midtom': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'hitom': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'rimshot': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'clap': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'hihat': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        },
-        'cymbal': {
-            'level': 1,
-            'attack': 0,
-            'decay': .5,
-            'distortion': 1
-        }
+    'snare': {
+      'level': 1,
+      'attack': 0,
+      'decay': 0.5,
+      'distortion': 1
     },
-
-    'rhythmSettings': {
-        '0': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': true
-        },
-        '1': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '2': {
-            'kick': false,
-            'snare': true,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': true
-        },
-        '3': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': true
-        },
-
-        '4': {
-            'kick': false,
-            'snare': true,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '5': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': true
-        },
-        '6': {
-            'kick': false,
-            'snare': true,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '7': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-
-        '8': {
-            'kick': false,
-            'snare': true,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '9': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '10': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '11': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-
-        '12': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '13': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '14': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        },
-        '15': {
-            'kick': false,
-            'snare': false,
-            'lowtom': false,
-            'midtom': false,
-            'hitom': false,
-            'rimshot': false,
-            'clap': false,
-            'hihat': false,
-            'cymbal': false
-        }
+    'lowtom': {
+      'level': 1,
+      'attack': 0,
+      'decay': 0.5,
+      'distortion': 1
+    },
+    'midtom': {
+      'level': 1,
+      'attack': 0,
+      'decay': 0.5,
+      'distortion': 1
+    },
+    'hitom': {
+      'level': 1,
+      'attack': 0,
+      'decay': 0.5,
+      'distortion': 1
+    },
+    'rimshot': {
+      'level': 0.8,
+      'attack': 0.5,
+      'decay': 1,
+      'distortion': 0
+    },
+    'clap': {
+      'level': 1,
+      'attack': 0.2,
+      'decay': 0.5,
+      'distortion': 0
+    },
+    'hihat': {
+      'level': 0.2,
+      'attack': 1,
+      'decay': 0,
+      'distortion': 1
+    },
+    'cymbal': {
+      'level': 1,
+      'attack': 0.5,
+      'decay': 0,
+      'distortion': 0
     }
-};
+  },
+  'rhythmSettings': {
+    '0': {
+      'kick': true,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': true,
+      'clap': false,
+      'hihat': true,
+      'cymbal': false
+    },
+    '1': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': false,
+      'cymbal': false
+    },
+    '2': {
+      'kick': false,
+      'snare': true,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': true,
+      'cymbal': false
+    },
+    '3': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': true,
+      'clap': false,
+      'hihat': true,
+      'cymbal': false
+    },
+    '4': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': true,
+      'clap': false,
+      'hihat': false,
+      'cymbal': false
+    },
+    '5': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': true,
+      'cymbal': false
+    },
+    '6': {
+      'kick': false,
+      'snare': true,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': false,
+      'cymbal': false
+    },
+    '7': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': false,
+      'cymbal': false
+    },
+    '8': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': true,
+      'clap': true,
+      'hihat': false,
+      'cymbal': false
+    },
+    '9': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': true,
+      'clap': true,
+      'hihat': false,
+      'cymbal': false
+    },
+    '10': {
+      'kick': false,
+      'snare': true,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': false,
+      'cymbal': false
+    },
+    '11': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': true,
+      'hihat': false,
+      'cymbal': false
+    },
+    '12': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': false,
+      'cymbal': false
+    },
+    '13': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': true,
+      'hihat': false,
+      'cymbal': false
+    },
+    '14': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': true,
+      'clap': true,
+      'hihat': false,
+      'cymbal': false
+    },
+    '15': {
+      'kick': false,
+      'snare': false,
+      'lowtom': false,
+      'midtom': false,
+      'hitom': false,
+      'rimshot': false,
+      'clap': false,
+      'hihat': false,
+      'cymbal': false
+    }
+  }
+}
 
 
 
 // 0 to 1 -- 1 is 100% : 0 is 0% 
+
+
