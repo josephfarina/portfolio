db1c1e782e84b26320b447b02c5d8a186dc01f19
Joseph Farina <Jrf61194@gmail.com>
2016-07-21 07:46:31 -0400

    buttons starting to work

--- app/projects/drum-machine/directives/drum-machine-button.directive.ts
+++ app/projects/drum-machine/directives/drum-machine-button.directive.ts
@@ -20,47 +20,48 @@ export class DrumMachineButtonDirective implements AfterViewInit {
     private button: any;
     private beat: any;
 
-    private activeIndication: Object = { active: 'red' };
+    private activeLight: Object = { active: 'red' };
     private beatIndication: Object = { active: 'yellow' };
 
     constructor(private _el: ElementRef) { this.el = _el.nativeElement; }
 
-    @HostListener('click') onClick() { this.flashBeat(); }
+    // @HostListener('click') onClick() { this.flashBeat(); }
 
 
     ngAfterViewInit() {
         this.s = Snap(this.el);
-        this.activeIndicator = this.s.select('#circle');
-        this.button = this.s.select('#button-front');
-        this.beat = this.s.select('#beat');
+        this.activeIndicator = this.s.select('circle');
 
-        this.activeIndication['inactive'] = this.activeIndicator.attr('fill');
-        this.beatIndication['inactive'] = this.beat.attr('fill');
 
-        this.s.hover(() => {
-            if (!this.isClicked) {
-                this.s.animate({ fill: 'none' }, 1000, this.s.mina.ease);
-                this.activeIndicator = this.s.select('circle');
-                this.activeIndicator.animate({ fill: this.activeIndication['active'], }, 100, this.s.mina.easein);
-            }
-        });
+        // this.button = this.s.selectAll();
 
-        this.s.mouseout(() => {
-            if (!this.isClicked) {
-                this.s.animate({ stroke: '' }, 1000, this.s.mina.ease);
-                this.activeIndicator.animate({ fill: this.activeIndication['inactive'], }, 100, this.s.mina.easeout);
-            }
-        });
+        this.activeLight['inactive'] = this.activeIndicator.attr('fill');
+
+        // this.s.hover(() => {
+            // if (!this.isClicked) {
+
+                // this.s.animate({ transform: 't0, -3' }, 200, mina.elastic);
+                // this.activeIndicator = this.s.select('circle');
+                // this.activeIndicator.animate({ fill: this.activeIndication['active'], }, 100, this.s.mina.easein);
+            // }
+        // });
+
+        // this.button.mouseout(() => {
+        //     if (!this.isClicked) {
+        //         this.button.animate({ stroke: '' }, 1000, this.s.mina.ease);
+        //         this.activeIndicator.animate({ fill: this.activeIndication['inactive'], }, 100, this.s.mina.easeout);
+        //     }
+        // });
 
         this.s.click(() => {
-            if (!this.isClicked) {
-                this.s.animate({ transform: 't0, -4' }, 300, this.s.mina.elastic);
-                this.activeIndicator.attr({ fill: this.activeIndication['active'] });
+            if (!this.isClicked) { 
+                this.s.animate({ transform: 't0, -3' }, 200, mina.elastic);
+                this.activeIndicator.animate({ fill: 'orange'}, 200, mina.easeinout);
             }
-            if (this.isClicked) {
-                this.s.animate({ transform: '' }, 300, this.s.mina.elastic);
+            if (this.isClicked) { 
+                this.s.animate({ transform: '' }, 200, mina.elastic);
+                this.activeIndicator.animate({ fill: 'blue'}, 200, mina.easeinout);
             }
-
             this.isClicked = !this.isClicked;
         });
     }
@@ -71,10 +72,10 @@ export class DrumMachineButtonDirective implements AfterViewInit {
         });
     }
 
-    flashBeat() {
-        this.beat.animate({ fill: this.beatIndication['active'] }, 0,
-            () => {this.beat.animate({ fill: this.beatIndication['inactive'] }, 250, this.s.mina.easeout); });
-    }
+    // flashBeat() {
+    //     this.beat.animate({ fill: this.beatIndication['active'] }, 0,
+    //         () => {this.beat.animate({ fill: this.beatIndication['inactive'] }, 250, this.s.mina.easeout); });
+    // }
 
 }
 
