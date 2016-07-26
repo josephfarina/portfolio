4bc63709d6830a21e6b290492c5752a6d6759548
Joseph <Jrf61194@gmail.com>
2016-07-25 21:56:31 -0400

    lots of progress

--- app/app-component/app.component.ts
+++ app/app-component/app.component.ts
@@ -22,6 +22,27 @@ export class AppComponent implements OnInit {
     private showEnabled: boolean = false;
     private isActive: boolean = false;
     private routeName: string;
+    private activeProject: Project;
+    private projects: Project[] = [
+        {
+            number: '01',
+            heading: 'Sequencer',
+            caption: 'UI Built with scss and makes use of bootstrap',
+            color: 'blue'
+        },
+        {
+            number: '02',
+            heading: 'Calculator',
+            caption: 'An 808 inspired sequencer built with angular2',
+            color: 'yellow'
+        },
+        {
+            number: '03',
+            heading: 'TicTacToe',
+            caption: 'Play a couple games, eh?',
+            color: 'purple'
+        }
+    ];
     constructor(
         private navTitleService: NavTitleService,
         private router: Router) {
@@ -34,6 +55,11 @@ export class AppComponent implements OnInit {
         this.showEnabled = false;
     }
 
+    setActiveProject(project: Project) {
+        console.log(project)
+        this.activeProject = project;
+    }
+
     toggleActive() {
         this.isActive = !this.isActive;
     }
@@ -41,3 +67,10 @@ export class AppComponent implements OnInit {
 
 // stupid hack to have a blank router at times when others arent selected
 @Component({ selector: 'blank', template: '' }) export class EmptyComponent { }
+
+export interface Project {
+    number: string;
+    heading: string;
+    caption: string;
+    color: string;
+}
