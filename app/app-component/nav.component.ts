b8e33ca58231c6ef74e3986438da4f02f34cbc91
Joseph <Jrf61194@gmail.com>
2016-07-28 20:49:56 -0400

    animations working well for nav

--- app/app-component/nav.component.ts
+++ app/app-component/nav.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ROUTER_DIRECTIVES, Router} from '@angular/router';
 import { NavTitleService } from './../shared/services/nav-title.service';
 
-
 @Component({
     directives: [ ROUTER_DIRECTIVES ],
     moduleId: module.id,
     providers: [NavTitleService],
     selector: 'my-nav',
     template: `
-    <nav class="animated fadeIn">
+    <nav (mouseenter)="isNewRoute = !isNewRoute"   [ngClass]="{ 'animated fadeInDownBig':  isNewRoute }">
         <div class="navigation-logo">
             <h5>{{routeName | uppercase}}</h5>
         </div>
@@ -23,10 +22,15 @@ import { NavTitleService } from './../shared/services/nav-title.service';
 
 export class NavComponent {
     private routeName: string = '';
+    private isNewRoute: boolean = false;
     constructor(
         private navTitleService: NavTitleService,
         private router: Router) {
         this.router.events.subscribe(route => {
+            this.isNewRoute = true;
+            setTimeout(() => {
+                 this.isNewRoute = false;
+             }, 1500);
             this.routeName = this.navTitleService.getCurrentUrl();
         });
     }
