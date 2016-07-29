1d526965a99707ae796bb887cf821b75c0aee3c0
Joseph <Jrf61194@gmail.com>
2016-07-29 19:12:40 -0400

    fixed heroku bug

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-
 var app = express();
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
