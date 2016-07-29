712ee17a20ab66f4a69a65597d3faf926a724236
Joseph <Jrf61194@gmail.com>
2016-07-29 19:10:19 -0400

    fc

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
